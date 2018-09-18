338679a606b35ce03da0ee3cedd369d7550ef4e4
brisk007 <35291027+brisk007@users.noreply.github.com>
2018-09-18 11:33:44 +0800

    Update 234. Palindrome Linked List.java

--- 234. Palindrome Linked List.java
+++ 234. Palindrome Linked List.java
@@ -1,3 +1,19 @@
+/*
+
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+
+*/
+
+// 利用慢指针和快指针找到链表的中心位置，同时将前半部分链表反转，最后判断两个链表的元素是否完全一样
 class Solution {
         public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
